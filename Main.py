039904d20dc129b4eaeabb73cf22cf23793ed839
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 23:17:08 +0000

    Almighty formula

--- Main.py
+++ Main.py
@@ -1,12 +1,19 @@
 # Exercise
-print("------------------------------------")
-print("Calculating the area of a rectangle")
-print("------------------------------------")
+import math
+print("--------------------------------------")
+print("The almighty formula for Quadratic eqn")
+print("--------------------------------------")
 
-length = float(input("Enter the length of the rectangle:"))
-print(f"length = {length}")
+# 2x^2 - 5x + 3
+# It is in the form ax^2 + bx + c
+a = int(input("Enter the coefficient of x^2: "))
 print()
-width = float(input("Enter the width of the rectangle:"))
-print(f"width = {width}")
+b = int(input("Enter the coefficient of x: "))
 print()
-print(f"The area of the rectangle is: area = {length * width}cm^2")
+c = int(input("Enter the constant: "))
+print()
+pos = -b + math.sqrt((b * b) - (4 * a * c))
+neg = -b - math.sqrt((b * b) - (4 * a * c))
+
+print(f"The first result = {pos / (2 * a)}")
+print(f"The second result = {neg / (2 * a)}")
