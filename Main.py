12e08ab434e4a624e916c945c3379a3023f87f63
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 23:57:11 +0000

    Hypotenuse of a right triangle

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 # Exercise
 import math
-print("----------------------------")
-print("Area of a circle")
-print("---------------------------")
-radius = float(input("Enter the radius: "))
-area = math.pi * pow(radius, 2)
-print(f"The area is {round(area, 3)}")
+print("------------------------------")
+print("Hypotenuse of a right triangle")
+print("-------------------------------")
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+hyp = math.sqrt(pow(a, 2) + pow(b, 2))
+print(f"The hypotenuse is {round(hyp, 3)}")
