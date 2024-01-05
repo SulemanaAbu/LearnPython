07483a9a5567aa1ae3375e982fa81c2a131af34f
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 22:09:46 +0000

    Variables and basics

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+print("Hello World!")
+
+age = 21
+print("You are "+str(age)+" years old")
+print(f"You are {age} years old")
+
+quantity = 32
+print("There are "+str(quantity)+" items available")
+
+gpa = 3.42
+print(f"My gpa is {gpa}")
+
+distance = 60.58
+print(f"You run {distance} km")
+
+name = "Abu"
+print(f"My name is {name}")
+
+online = True
+print(f"Are you online? {online}")
