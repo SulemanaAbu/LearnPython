7d3d36178005475429f8fc5d9f811b3f7b6644e5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 23:37:30 +0000

    circumference of a circle

--- Main.py
+++ Main.py
@@ -1,19 +1,7 @@
 # Exercise
 import math
-print("--------------------------------------")
-print("The almighty formula for Quadratic eqn")
-print("--------------------------------------")
+print("----------------------------")
+print("Circumference of a circle")
+print("---------------------------")
 
-# 2x^2 - 5x + 3
-# It is in the form ax^2 + bx + c
-a = int(input("Enter the coefficient of x^2: "))
-print()
-b = int(input("Enter the coefficient of x: "))
-print()
-c = int(input("Enter the constant: "))
-print()
-pos = -b + math.sqrt((b * b) - (4 * a * c))
-neg = -b - math.sqrt((b * b) - (4 * a * c))
-
-print(f"The first result = {pos / (2 * a)}")
-print(f"The second result = {neg / (2 * a)}")
+print(f"Circumference  = {2 * math.pi * float(input("Enter the radius of the circle: "))}")
