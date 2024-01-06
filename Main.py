87f9abecdfbbd09bcf0a201afea0051cd2a4236b
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-06 14:21:47 +0000

    while loop

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
-# format specifiers
-a = int(input("Enter the coefficient of x^2: "))
-b = int(input("Enter the coefficient of x: "))
-c = int(input("Enter the constant: "))
+name = input("Enter your name: ")
+while name == "":
+    name = input("Enter your name: ")
+print(f"Hello {name}")
 
-print(f"The quadratic equation is {a: }x^2{b:+}x{c:+}")
