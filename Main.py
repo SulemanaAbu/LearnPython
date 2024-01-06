a3d70cae4690694a08d83c410dce5c8907f7e00b
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-06 22:34:06 +0000

    shopping cart program

--- Main.py
+++ Main.py
@@ -1,6 +1,21 @@
-# Lists, sets and Tuple
-fruits = ("banana", "mango", "apple", "pineapple", "water melon")
-print(fruits.index("apple"))
-for fruit in fruits:
-    print(fruit)
+# Shopping cart program
+foods = []
+prices = []
+total = 0
 
+while True:
+    food = input("Enter the food you want to buy (q or Q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+print("------ Your Cart----------")
+for food in foods:
+    print(food, end=" ")
+print()
+for price in prices:
+    total += price
+print()
+print(f"Your total balance is {total}")
