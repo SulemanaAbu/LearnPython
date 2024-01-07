ae39d559cd95c7a421748f9aa4be68726d95b4b5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-07 05:22:06 +0000

    random guessing game

--- Main.py
+++ Main.py
@@ -1,28 +1,20 @@
-# Dictionaries in python
-menu = {"coca cola": 1.65,
-        "fried rice": 65,
-        "cocktail": 12,
-        "pizza": 35,
-        "popcorn": 23}
-
-cart = []
-total = 0
-
-print("----------MENU---------------")
-for key, value in menu.items():
-    print(f"{key:10} : ${value:.2f}")
-print("----------MENU---------------")
+# Random number
+import random
+low = 1
+high = 100
+guesses = 0
+number = random.randint(low, high)
 
 while True:
-    food = input("Select the food you want to buy (q or Q to quit): ").lower()
-    if food == "q":
-        break
-    elif menu.get(food) is not None:
-        cart.append(food)
-print("----------YOUR ORDER---------")
-for food in cart:
-    total += menu.get(food)
-    print(food, end=" ")
-print()
-print("----------YOUR TOTAL---------")
-print(f"Total = ${total:.2f}")
+     guess = int(input(f"Enter a number between {low} - {high}: "))
+     guesses += 1
+
+     if guess < number:
+         print("GUESS IS TOO LOW!")
+     elif guess > number:
+         print("GUESS IS TOO HIGH!")
+     else:
+         print("GUESS IS CORRECT!")
+         break
+
+print(f"This round took you {guesses} guesses")
