f3b7b8adb3054856331123262b1bd64dc10a8003
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-07 05:48:41 +0000

    rock paper scissors

--- Main.py
+++ Main.py
@@ -1,20 +1,36 @@
 # Random number
 import random
-low = 1
-high = 100
-guesses = 0
-number = random.randint(low, high)
+options = ("ROCK", "PAPER", "SCISSORS")
+playing = True
+while playing:
+    player = None
+    computer = random.choice(options)
 
-while True:
-     guess = int(input(f"Enter a number between {low} - {high}: "))
-     guesses += 1
+    while player not in options:
+        player = input("Enter your choice (Rock, Paper or Scissors): ").upper()
 
-     if guess < number:
-         print("GUESS IS TOO LOW!")
-     elif guess > number:
-         print("GUESS IS TOO HIGH!")
-     else:
-         print("GUESS IS CORRECT!")
-         break
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
 
-print(f"This round took you {guesses} guesses")
+    if player == computer:
+        print("IT's A TIE")
+    elif player == "ROCK" and computer == "SCISSORS":
+        print("YOU WIN!")
+    elif player == "ROCK" and computer == "PAPER":
+        print("YOU LOSE!")
+    elif player == "PAPER" and computer == "SCISSORS":
+        print("YOU LOSE!")
+    elif player == "PAPER" and computer == "ROCK":
+        print("YOU WIN!")
+    elif player == "SCISSORS" and computer == "ROCK":
+        print("YOU LOSE!")
+    else:
+        print("YOU WIN!")
+        break
+    if not input("Play again? (Y?N): ").lower() == "y":
+        playing = False
+    else:
+        playing = True
+
+print("Thanks for playing!")
+
