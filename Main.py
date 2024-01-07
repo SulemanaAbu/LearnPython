63a155cb6fa86f66cb92d3b2729553d2f63a57d6
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-07 04:20:58 +0000

    Dictionaries

--- Main.py
+++ Main.py
@@ -1,21 +1,4 @@
-# Shopping cart program
-foods = []
-prices = []
-total = 0
-
-while True:
-    food = input("Enter the food you want to buy (q or Q to quit): ")
-    if food.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
-print("------ Your Cart----------")
-for food in foods:
-    print(food, end=" ")
-print()
-for price in prices:
-    total += price
-print()
-print(f"Your total balance is {total}")
+# Dictionaries in python
+capitals = {"USA": "Washington DC",
+            "China": "Beijing",
+            "Ghana": "Accra"}
