435fe54c28100175adffd2d343230beebe83ca69
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-08 10:39:46 +0000

    default arguments

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
-# Default arguments
-
-def net_price(list_price, discount=0, tax=0.05):
-    return list_price * (1 - discount) * (1 + tax)
+import time
 
 
-print(net_price(500, 0.1, 0))
+def count(end, start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE!")
+
+# count(10)
+
+
+count(30, 15)
