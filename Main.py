977e2cbdbe171dfc42510e559245f17e2907e7e5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-08 10:33:22 +0000

    default arguments

--- Main.py
+++ Main.py
@@ -1,12 +1,7 @@
-# Functions in python
-def intro(full_name, age, residence, school):
-    full_name = full_name.capitalize()
-    print(f"Hello {full_name}")
-    print(f"You are {age} years old")
-    print(f"You reside at {residence}")
-    print(f"You attend {school}")
+# Default arguments
+
+def net_price(list_price, discount=0, tax=0.05):
+    return list_price * (1 - discount) * (1 + tax)
 
 
-intro("salman Isak", 20, "Madina", "University of Ghana")
-print()
-intro("Richmond Eng", 42, "Denver", "University of Hong Kong")
+print(net_price(500, 0.1, 0))
