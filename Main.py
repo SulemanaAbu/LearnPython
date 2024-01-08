565d5e84fd969ee077d18247d65d5b0119cf21cb
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-08 21:44:40 +0000

    Abstraction

--- Main.py
+++ Main.py
@@ -1,13 +1,45 @@
-from fish import Fish
-from hawk import Hawk
-from rabbit import Rabbit
+from abc import abstractmethod
+
+
+class Animal:
+
+    @abstractmethod
+    def eat(self):
+        return "This animal is eating"
+
+    @abstractmethod
+    def move(self):
+        return "This animal is moving"
+
+
+class Fish(Animal):
+    def eat(self):
+        return "This fish is eating"
+
+    def move(self):
+        return "This fish is moving"
+
+
+class Hawk(Animal):
+    def eat(self):
+        return "This hawk is eating"
+
+    def move(self):
+        return "This hawk is moving"
+
+
+class Rabbit(Animal):
+    def eat(self):
+        return "This rabbit is eating"
+
+    def move(self):
+        return "This rabbit is moving"
+
 
 fish = Fish()
 hawk = Hawk()
 rabbit = Rabbit()
-
-
-print(rabbit.run())
-print(hawk.fly())
-print(fish.swim())
-print(fish.sleep())
+print(rabbit.eat())
+print(fish.move())
+print(hawk.eat())
+print(hawk.move())
